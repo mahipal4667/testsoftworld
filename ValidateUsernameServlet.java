068fdc0637e9b2942dbfe8640ca4eaf694d900d6
mahipal4667 <43143536+mahipal4667@users.noreply.github.com>
2018-10-08 09:56:30 -0500

    Add files via upload

---
+++ ValidateUsernameServlet.java
@@ -0,0 +1,48 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ValidateUsernameServlet
+ */
+@WebServlet("/validateserv")
+public class ValidateUsernameServlet extends HttpServlet {
+	
+	private List<String> usernames = new ArrayList<String>();
+	
+	@Override
+		public void init(ServletConfig config) throws ServletException {
+			// TODO Auto-generated method stub
+			super.init();
+			usernames.add("Mahi");
+			usernames.add("Raju");
+			usernames.add("Kumar");
+			usernames.add("Hari");
+		}
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String uname = request.getParameter("username");
+		PrintWriter out = response.getWriter();
+		
+		if(usernames.contains(uname)) {
+			out.println("<font color='red'>Username taken</font>");	
+		}else {
+			out.println("<font color='green'>Username available</font>");
+		}
+		
+	}
+
+}
